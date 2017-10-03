48f35c29f0c5af69bc28b7a5cd5a881d83052aa7
Zenia <zeniavilla@hotmail.com>
2017-10-03 12:51:58 -0700

    Remove https from domain name in CORS url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['localhost:3000', 'https://collab-create.herokuapp.com/']
+    origins ['localhost:3000', 'collab-create.herokuapp.com']
 
     resource '*',
       headers: :any,
